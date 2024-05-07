a53750994ae109107ed800eeb2bf929adf861e03
Chamath Anjula <124298134+chamathAn@users.noreply.github.com>
2024-05-07 20:43:02 +0530

    added role to get back the role in frontend

--- middlewares/checkAdmin.js
+++ middlewares/checkAdmin.js
@@ -1,7 +1,7 @@
 const checkAdmin = (req, res, next) => {
   if (req.auth.claims.metadata.role !== "admin") {
     
-    return next(createError(401, "Unauthorized")); 
+    return next(createError(401, "Unauthorized" + req.auth.claims.metadata.role)); 
   }
   next(); 
 };
