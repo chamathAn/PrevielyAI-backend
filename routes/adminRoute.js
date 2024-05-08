205766361ae308819e66db4b9576ea1464f1af7d
Chamath Anjula <124298134+chamathAn@users.noreply.github.com>
2024-05-08 21:38:33 +0530

    removed middleswares belongs to clerk only from routes.

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,9 +4,9 @@ const checkAdmin = require("../middlewares/checkAdmin");
 
 const route = require("express").Router();
 
-route.post("/postjob",authonticateToken, checkAdmin, adminJobPost);
-route.post("/alljobs", authonticateToken, checkAdmin, adminGetAllJobs);
-route.post("/allapplications", authonticateToken, checkAdmin, adminGetAllApplications);
-route.post("/job/application/:id", authonticateToken, checkAdmin, adminGetJobApllication);
+route.post("/postjob", adminJobPost);
+route.post("/alljobs", adminGetAllJobs);
+route.post("/allapplications", adminGetAllApplications);
+route.post("/job/application/:id", adminGetJobApllication);
 
 module.exports = route;
